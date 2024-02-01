1409ce7d4bdda5de6f5601e6c2a00ef1ad050bbf
marialuiza2a <mluizasenai2@gmail.com>
2024-02-01 04:51:56 -0300

    feat: finishOrder

--- src/pages/app/views/my-cart-page.tsx
+++ src/pages/app/views/my-cart-page.tsx
@@ -1,19 +1,40 @@
 import { ProductCardCartItem } from "@/components/interface/cards/product-card-cart";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 export function MyCartPage() {
   return (
     <>
       <ProductCardCartItem />
       <ProductCardCartItem />
-
-      <div className="flex justify-between items-center border-t-2 py-2">
-        <p className="text-lg font-semibold">Total:</p>
-        <p className="text-lg font-semibold">$40.00</p>
+      <div className="flex flex-col border-t-2 pt-5 gap-5">
+        <div className="flex items-center justify-between">
+          <h1 className="font-bold">Pagamento pelo app</h1>
+          <Link to="#" className="text-red-500">
+            Trocar
+          </Link>
+        </div>
+        <div className="flex items-center pb-3 gap-3">
+          <img src="/pix.webp" alt="Pix Method Payment" className="max-w-8" />
+          <h1 className="font-medium">PIX</h1>
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        <div className="flex justify-between items-center border-t-2 pt-5">
+          <p className="text-md font-semibold">Total:</p>
+          <p className="text-md font-semibold">R$ 40.00</p>
+        </div>
+        <div className="flex justify-between items-center">
+          <p className="text-md font-semibold">Desconto do clube:</p>
+          <p className="text-md font-semibold">R$ 0.00</p>
+        </div>
+        <div className="flex justify-between items-center">
+          <p className="text-md font-semibold">Subtotal:</p>
+          <p className="text-md font-semibold">R$ 35.00</p>
+        </div>
       </div>
 
-      <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 focus:outline-none">
-        Finalizar Compra
-      </button>
+      <Button>Finalizar compra</Button>
     </>
   );
 }
