976c9fa1dab43613ac3302033dfa511b567a06f2
marialuiza2a <mluizasenai2@gmail.com>
2024-01-31 22:25:44 -0300

    feat: finish

--- src/pages/_layouts/view.tsx
+++ src/pages/_layouts/view.tsx
@@ -1,16 +1,11 @@
 import { HeaderBack } from "@/components/interface/header-back";
 import { Outlet } from "react-router-dom";
-import { tabs } from "./tabs";
-
-import MobileTabBar from "@/components/ui/tab-bar";
-
 export function ViewLayout() {
   return (
     <>
       <HeaderBack />
       <div className="flex flex-1 flex-col gap-4 p-5 pt-6">
         <Outlet />
-        <MobileTabBar tabs={tabs} />
       </div>
     </>
   );
