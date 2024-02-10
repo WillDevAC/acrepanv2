9c602a845994a009a3c62cf8ce760708b3e38bb0
marialuiza2a <mluizasenai2@gmail.com>
2024-02-10 17:18:48 -0300

    feat: finish"

--- src/pages/app/home/home.tsx
+++ src/pages/app/home/home.tsx
@@ -28,7 +28,7 @@ export function HomePage() {
 
   return (
     <>
-      <section className="pr-4 pl-4 flex items-center justify-around w-full bg-white h-11">
+      <section className="pr-4 pl-4 flex items-center justify-between w-full bg-white h-11">
         <div className="flex items-center gap-3">
           <Clock10 />
           <div className="flex flex-col">
