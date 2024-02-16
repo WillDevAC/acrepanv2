8899674fe94e4aa7bd281af97e855f8b810e6334
marialuiza2a <mluizasenai2@gmail.com>
2024-02-16 04:20:35 -0300

    finish

--- src/pages/app/home/home.tsx
+++ src/pages/app/home/home.tsx
@@ -4,7 +4,6 @@ import { ProductCard } from "@/components/interface/cards/product-card";
 import { useGetProducts } from "@/api/get-all-products";
 
 import { useState, useRef, useEffect } from "react";
-import { ViewProductDetailsDrawer } from "@/components/interface/drawer/view-product-drawer";
 
 const inactiveTabStyle =
   "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300";
@@ -71,18 +70,16 @@ export function HomePage() {
         {!isLoading &&
           listProducts &&
           listProducts.map((product: any) => (
-            <ViewProductDetailsDrawer>
-              <ProductCard
-                key={product.id}
-                type="default"
-                id={product.id}
-                title={product.title}
-                priceAtacado={product.priceAtacado}
-                priceVarejo={product.priceAtacado}
-                description={product.description}
-                img={product.img.url}
-              />
-            </ViewProductDetailsDrawer>
+            <ProductCard
+              key={product.id}
+              type="default"
+              id={product.id}
+              title={product.title}
+              priceAtacado={product.priceAtacado}
+              priceVarejo={product.priceAtacado}
+              description={product.description}
+              img={product.img.url}
+            />
           ))}
       </section>
     </main>
@@ -122,39 +119,17 @@ export function HomePage() {
         {!isLoading &&
           listProducts &&
           listProducts.map((product: any) => (
-            <ViewProductDetailsDrawer>
-              <ProductCard
-                key={product.id}
-                type="list"
-                id={product.id}
-                title={product.title}
-                priceAtacado={product.priceAtacado}
-                priceVarejo={product.priceAtacado}
-                description={product.description}
-                img={product.img.url}
-              />
-            </ViewProductDetailsDrawer>
+            <ProductCard
+              key={product.id}
+              type="list"
+              id={product.id}
+              title={product.title}
+              priceAtacado={product.priceAtacado}
+              priceVarejo={product.priceAtacado}
+              description={product.description}
+              img={product.img.url}
+            />
           ))}
-          <h1>EAE</h1>
-          <h1>EAE</h1>
-          <h1>EAE</h1>
-          <h1>EAE</h1>
-          <h1>EAE</h1>
-          <h1>EAE</h1>
-          <h1>EAE</h1>
-          <h1>EAE</h1>
-          <h1>EAE</h1>
-          <h1>EAE</h1>
-          <h1>EAE</h1>
-          <h1>EAE</h1>
-          <h1>EAE</h1>
-          <h1>EAE</h1>
-          <h1>EAE</h1>
-          <h1>EAE</h1>
-          <h1>EAE</h1>
-          <h1>EAE</h1>
-          <h1>EAE</h1>
-          <h1>EAE</h1>
       </div>
     </main>
   );
@@ -162,7 +137,10 @@ export function HomePage() {
   return (
     <>
       <DeliveryShippingInfo time="15 à 20 minutos" isOpenCompany={true} />
-      <div className="bg-white border-b-2 sticky top-14 z-10 overflow-x-auto w-full" aria-label="Tabs">
+      <div
+        className="bg-white border-b-2 sticky top-14 z-10 overflow-x-auto w-full"
+        aria-label="Tabs"
+      >
         <div className="flex" style={{ overflowX: "auto" }}>
           {renderTabButton("destaques", "Destaques")}
           {renderTabButton("Pão", "Pães")}
